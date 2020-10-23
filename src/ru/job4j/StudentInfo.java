16d0e5326f3155292a342c16280bb3e30af37958
AgaevAlex <agaevalex97@gmail.com>
2020-10-23 22:53:19 +0300

    3.3. Разбор первой программы. [#253312]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Alexey Agaev");
+        System.out.println("19.06.1997");
+    }
+}
